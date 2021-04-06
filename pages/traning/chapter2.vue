cd25a02f6ae0379251591888faf40664f1fc1dd2
hayato.oishi <hayato.oishi@northdetail.co.jp>
2021-04-06 19:29:18 +0900

    test

--- pages/traning/chapter2.vue
+++ pages/traning/chapter2.vue
@@ -153,13 +153,13 @@ export default {
     title() {
       return 'Chpater2'
     },
-    loginButton() {
-      if (this.isLogin) {
-        return 'ログアウト'
-      } else {
-        return 'ログイン'
-      }
-    },
+    // loginButton() {
+    //   if (this.isLogin) {
+    //     return 'ログアウト'
+    //   } else {
+    //     return 'ログイン'
+    //   }
+    // },
   },
   methods: {
     login() {
