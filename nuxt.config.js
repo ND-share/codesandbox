709f83733d906de7245fe1036b914301a8091d3e
nd_lab <hayato.oishi@northdetail.co.jp>
2021-10-13 09:42:59 +0900

    clean codesandboxをdevで立ち上げるため一時的に修正
    t#

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,13 +16,9 @@ const config = () => {
       link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
 
-    server: {
-      port: process.env.NODE_ENV === 'development' ? 2999 : 3000,
-    },
-
-    env: {
-      ENV: process.env.NODE_ENV,
-    },
+    // server: {
+    //   port: process.env.NODE_ENV === 'development' ? 2999 : 3000,
+    // },
 
     // SSR検証用
     ssr: true,
